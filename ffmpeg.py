a0ed761fb69fdd889754e1a6479d14dc0e3d858d
wancheng1990 <25022549@qq.com>
2021-06-11 18:19:42 +0800

    add files

---
+++ ffmpeg.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+import os
+
+args = [
+    'extra-cflags="-c -Oz -fPIC"',
+    'cc=emcc',
+    'cxx=em++',
+    'ar=emar',
+    'ranlib=emranlib',
+    'prefix=./obj',
+    'target-os=none',
+    'arch=x86_64',
+    'cpu=generic'
+    ]
+
+    disables = [
+        'everything',
+        'all',
+        'avdevice',
+        'avformat',
+        'postproc',
+        'avfilter',
+        'programs',
+        'asm',
+        'parsers',
+        'muxers',
+        'demuxers',
+        'filters',
+        'doc',
+        'devices',
+        'network',
+        'bsfs',
+        'shared',
+        'hwaccels',
+        'debug',
+        'protocols',
+        'indevs',
+        'outdevs',
+        'runtime-cpudetect'
+    ]
+
+    enables = [
+        'small',
+        'cross-compile',
+        'gpl',
+        'avcodec',
+        'swresample',
+        'pthreads',
+        'ffmpeg',
+        'parser=h264',
+        'parser=hevc',
+        'parser=aac',
+        'decoder=h264',
+        'decoder=hevc',
+        'decoder=aac',
+        'decoder=pcm_alaw',
+        'decoder=pcm_mulaw',
+        'encoder=pcm_alaw',
+        'encoder=pcm_mulaw',
+        'encoder=aac',
+        'static'
+    ]
+
+    enables = ['enable-'+item for i,item in enumerate(enables)]
+    disables = ['disable-'+item for i,item in enumerate(disables)]
+    os.system('emconfigure ./ffmpeg/configure --' +
+              (' --'.join(args+disables+enables)))
+    os.system('make clean && emmake make -j && make install')
---
+++ ffmpeg/libavcodec/pcm.d
@@ -0,0 +1,15 @@
+libavcodec/pcm.o: libavcodec/pcm.c config.h libavutil/attributes.h \
+  libavutil/float_dsp.h libavutil/thread.h libavcodec/avcodec.h \
+  libavutil/samplefmt.h libavutil/avutil.h libavutil/common.h \
+  libavutil/macros.h libavutil/version.h libavutil/avconfig.h \
+  libavutil/intmath.h libavutil/mem.h libavutil/error.h \
+  libavutil/internal.h libavutil/timer.h libavutil/log.h libavutil/cpu.h \
+  libavutil/dict.h libavutil/pixfmt.h libavutil/libm.h \
+  libavutil/intfloat.h libavutil/mathematics.h libavutil/rational.h \
+  libavutil/buffer.h libavutil/channel_layout.h libavutil/frame.h \
+  libavutil/hwcontext.h libavcodec/bsf.h libavcodec/codec_id.h \
+  libavcodec/codec_par.h libavcodec/packet.h libavcodec/version.h \
+  libavcodec/codec.h libavcodec/codec_desc.h libavcodec/bytestream.h \
+  libavutil/avassert.h libavutil/intreadwrite.h libavutil/bswap.h \
+  libavcodec/internal.h libavutil/fifo.h libavcodec/mathops.h \
+  libavutil/reverse.h libavcodec/pcm_tablegen.h
